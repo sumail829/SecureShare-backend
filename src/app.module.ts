2a049a202d320de96f2ff923d6dd7410c2184a1b
sumal829 <samirpanjiyar4@gmail.com>
2026-05-18 12:33:29 +0545

    added modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
+import { FilesModule } from './files/files.module';
+import { SharesModule } from './shares/shares.module';
+import { PermissionsModule } from './permissions/permissions.module';
 
 
 
@@ -20,7 +24,15 @@ TypeOrmModule.forRoot({
   },
   autoLoadEntities: true,
   synchronize: true, // use false in production
-})
+}),
+
+UsersModule,
+
+FilesModule,
+
+SharesModule,
+
+PermissionsModule
   ],
   controllers: [AppController],
   providers: [AppService],
