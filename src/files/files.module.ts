504bd696d2f73ab0f876a139b07aacb3d5ae6daf
sumal829 <samirpanjiyar4@gmail.com>
2026-05-29 13:43:59 +0545

    setup module

--- src/files/files.module.ts
+++ src/files/files.module.ts
@@ -1,4 +1,10 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Share } from 'src/shares/entities/share.entity';
 
-@Module({})
+@Module({
+    imports: [TypeOrmModule.forFeature([File,Share])]
+})
+
+
 export class FilesModule {}
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,9 +3,9 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { UsersService } from './user.service';
 import { UsersController } from './user.controller';
-
+import { File } from '../files/entities/file.entity';
 @Module({
-  imports: [TypeOrmModule.forFeature([User]),
+  imports: [TypeOrmModule.forFeature([User,File]),
     
   ],
   providers: [UsersService],
